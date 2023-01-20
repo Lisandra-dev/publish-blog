bf400ac757bb409a0feb5f2151b0b1bbfef1fded
Lisandra Simonetti <mara_li@icloud.com>
2023-01-20 13:05:47 +0000

    fix js

--- docs/assets/js/utils.js
+++ docs/assets/js/utils.js
@@ -81,6 +81,7 @@ var p_img = /\.+\\/gi
 var img = document.querySelectorAll('img');
 for (var i = 0; i < img.length; i++) {
     var regAlt = new RegExp('\\|');
+    // image can only be rized with the `|*x*` syntax
     if (img[i].alt.match(regAlt)) {
         const alt = img[i].alt.split('|')
         for (var part of alt) {
@@ -92,12 +93,7 @@ for (var i = 0; i < img.length; i++) {
                 img[i].alt = img[i].alt.replace(partReg, '')
             }
         }
-    } else if (img[i].alt.match(new RegExp('\\d+', 'g'))) {
-        var size = getHeightWidth(img[i].alt)
-        img[i].width = size[0] > 0 ? size[0] : img[i].width
-        img[i].height = size[1] > 0 ? size[1] : img[i].height
-        img[i].alt = "";
-    }
+    } 
     var link = UrlExists(img[i], 1);
 }
 
